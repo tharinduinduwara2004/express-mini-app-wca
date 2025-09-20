7079f513cc010f85ac8b686a57b77fc3f53f28ef
tharinduinduwara2004 <tinduwara427@gmail.com>
2025-09-20 22:20:51 +0530

    Update app routes: comment out send message endpoint, add webhook message handling, and change health check reponse to 'Tharindu'

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,12 @@ app.use(express.json());
 const messageController = new MessageController();
 const webhookController = new WebhookController();
 
-app.post("/send-message", messageController.sendMessage);
+//app.post("/send-message", messageController.sendMessage);
 
+//to subscript webhook
 app.get("/webhook", webhookController.webhook);
+
+//to handle receiving messages
 app.post("/webhook", webhookController.webhookMessage);
 
 app.get('/helth',(req,res)=>{
